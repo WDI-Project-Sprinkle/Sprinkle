73fba91862245f644e913a984526f64a1a114fe9
Jimmy Lin <usmcjimmy@gmail.com>
2016-03-17 16:53:58 -0400

    jl: updated the schema with new indeed & career table with an app joint table

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,12 @@
 
-DROP TABLE IF EXISTS jobs CASCADE;
-DROP TABLE IF EXISTS applications CASCADE;
+DROP TABLE IF EXISTS indeed_jobs CASCADE;
+DROP TABLE IF EXISTS career_jobs CASCADE;
+DROP TABLE IF EXISTS app CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 
 
-CREATE TABLE jobs (
-  job_id INTEGER PRIMARY KEY,
+CREATE TABLE indeed_jobs (
+  indeed_id SERIAL UNIQUE PRIMARY KEY,
   company VARCHAR(255),
   job_title VARCHAR(255),
   job_desc VARCHAR(255),
@@ -13,15 +14,27 @@ CREATE TABLE jobs (
   state VARCHAR(255),
   salaries INTEGER,
   first_added TIMESTAMP,
-  indeed_id VARCHAR(255),
-  career_id VARCHAR(255),
-  indeed_url VARCHAR(255),
-  career_url VARCHAR(255)
+  job_id VARCHAR(255),
+  url VARCHAR(255)
+);
+
+CREATE TABLE career_jobs (
+  career_id SERIAL UNIQUE PRIMARY KEY,
+  company VARCHAR(255),
+  job_title VARCHAR(255),
+  job_desc VARCHAR(255),
+  city VARCHAR(255),
+  state VARCHAR(255),
+  salaries INTEGER,
+  first_added TIMESTAMP,
+  job_id VARCHAR(255),
+  url VARCHAR(255)
 );
 
 CREATE TABLE apps (
   profile_id SERIAL UNIQUE PRIMARY KEY,
-  job_id INTEGER REFERENCES jobs,
+  indeed_id INTEGER REFERENCES indeed_jobs,
+  career_id INTEGER REFERENCES career_jobs,
   saved_date TIMESTAMP,
   applied BOOLEAN
 );
