69ad887d93cc41020123dbe9d18cabf284a438ed
Jason Seminara <jasonseminara@users.noreply.github.com>
2016-03-18 10:54:20 -0400

    added util function for transforming an objects
    creates a copy of the object with consolidated children instead of arrays.

--- routes/search.js
+++ routes/search.js
@@ -12,6 +12,18 @@ search.route('/')
   res.send(res.data)
 })
 
+// @author Jason Seminara 2016-03-18
+// @description This will take an object whose vals are arrays; pluck off the first item and save it as the new val
+// @returns a copy of the original
+Object.prototype.pluckFirstArrayItem = function(first_argument) {
+  return Object.keys(this).reduce((copy,key)=>{
+    copy[key]=this[key][0]
+    return copy;
+  }
+  ,{})
+};
+
+
 function getJobs(req, res, next) {
   request(url + 'publisher=' + apikey + '&q=' + req.query.data.q + '&l=' + req.query.data.l + '&co=us&jt=' + req.query.data.jt + '&format=json&limit=20&v=2', function(err, response, body) {
     var data = JSON.parse(body);
@@ -20,5 +32,8 @@ function getJobs(req, res, next) {
     next()
   });
 }
+
+
+
 
 module.exports = search;
