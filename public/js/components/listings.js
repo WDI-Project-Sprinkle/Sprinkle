6568876460192b74306f0d3c6496bb456afdcdf6
Elton Cheng <eltoncheng86@gmail.com>
2016-03-18 16:56:54 -0400

    EC not yet implemented. trying to add listings to this component

--- public/js/components/listings.js
+++ public/js/components/listings.js
@@ -2,7 +2,16 @@ const React = require( 'react' );
 
 const Listings = React.createClass({
   render() {
-    return <h1>Detail Listings</h1>
+    return (
+      <li>
+        {this.props.company}
+        {this.props.role}
+        {this.props.desc}
+        {this.props.city}
+        {this.props.state}
+        {this.props.url}
+      </li>
+    )
   }
 })
 
