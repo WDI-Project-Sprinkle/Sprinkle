3d2aade90bc6cf3efaebe562947b20bc31e66f4a
Elton Cheng <eltoncheng86@gmail.com>
2016-03-19 18:46:37 -0400

    EC changed job listings to a listing component

--- public/js/components/listings.js
+++ public/js/components/listings.js
@@ -1,16 +1,30 @@
 const React = require( 'react' );
 
 const Listings = React.createClass({
+  handleSubmit : function(event) {
+    event.preventDefault();
+    this.props.saveJob();
+  },
+
   render() {
     return (
-      <li>
-        {this.props.company}
-        {this.props.role}
-        {this.props.desc}
-        {this.props.city}
-        {this.props.state}
-        {this.props.url}
-      </li>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          <li>
+            <strong>Company Name:</strong> {this.props.company}
+          </li>
+          <li>
+            <strong>Job Title:</strong> {this.props.role}
+          </li>
+          <li>
+            <strong>Job Description:</strong> {this.props.desc}
+          </li>
+          <li>
+            <a href={this.props.url} target="_blank"><strong>{this.props.name}</strong></a>
+          </li>
+          <button type="submit"> Save Job </button>
+        </ul>
+      </form>
     )
   }
 })
