632e134908280ef536985411a3970a913dd8a921
Elton Cheng <eltoncheng86@gmail.com>
2016-05-21 17:49:01 -0400

    refactored Login component to es6 syntax. Need to test

--- public/js/components/nav_components/login.js
+++ public/js/components/nav_components/login.js
@@ -1,4 +1,66 @@
-const React = require('react');
+import $ from 'jquery'; // requires jQuery for AJAX request
+import React, {Component} from 'react';
+import {browserHistory, Link} from 'react-router';
+
+// export default class Login extends Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = { // sets initial states
+//       email : '',
+//       password : '',
+//       error : ''
+//     }
+//   }
+//
+//   render() {
+//     if (localStorage.token) { // checks if logged in
+//       return <h3>Welcome {this.state.email}!</h3>
+//     }
+//     return (
+//       <form onSubmit={this.handleSubmit.bind(this)}>
+//         <input
+//           value={this.state.email}
+//           onChange={event => this.onEmailChange(event.target.value)}
+//           type="email"
+//           placeholder="email" />
+//           <br />
+//         <input
+//           value={this.state.password}
+//           onChange={event => this.onPasswordChange(event.target.value)}
+//           type="password"
+//           placeholder="password" />
+//           <br />
+//         <button type="submit">Submit</button>
+//         <h5>{this.state.error}</h5>
+//       </form>
+//     )
+//   }
+//   onEmailChange(email) {
+//     this.setState({email}); // updates username state
+//   }
+//   onPasswordChange(password) {
+//     this.setState({password}); // updates password state
+//   }
+//   handleSubmit(event) {
+//     event.preventDefault(); // prevents page from refreshing
+//     let error = 'Oops, please check your email or password';
+//     if (this.state.email == '' || this.state.password == '') { // checks for real email/password
+//       this.setState({error:error})
+//     }
+//     $.post('/users/login', { // AJAX post request to users/login route
+//       email : this.state.email,
+//       password : this.state.password
+//     })
+//     .done((data) => {
+//       if (data.agent == 'error') { // if username/password doesn't match
+//         this.setState({error:error})
+//       } else { // if login is successful
+//         localStorage.token = data.token; // saves token to local storage
+//       }
+//     })
+//   }
+// }
 
 const Login = React.createClass({
   handleLogin : function( event ) {
